ea5e95146ab32c802baa75ccc1af561450e7955d
Jacob Allen <jedijake@me.com>
2018-06-05 23:01:27 -0500

    Test to ensure node-sass and npm-run-all work, refactor Home component into its own folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Home from './Home'
+import Home from './components/home/Home'
+import Fake from './components/fake/Fake'
 import NotFound from './NotFound'
 
 class App extends Component {
