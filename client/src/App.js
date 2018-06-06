8bcb523de6e9c19058445cbce141f6398cd7505e
Jacob Allen <jedijake@me.com>
2018-06-06 00:03:19 -0500

    Import Google fonts, scaffold SASS file structure, test to ensure proper compiling

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './components/home/Home'
-import Fake from './components/fake/Fake'
 import NotFound from './NotFound'
 
 class App extends Component {
