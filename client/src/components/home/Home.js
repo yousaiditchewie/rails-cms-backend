b048e0f8b8335beaf4f726b2de4b61e94eb358e8
Jacob Allen <jedijake@me.com>
2018-06-14 22:40:05 -0500

    Prototype the home page

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -6,18 +6,29 @@ export default class Home extends Component {
   render() {
     return (
       <section id="hero" className="Hero">
-        <h1 className="Hero-logo">
-          <img className="Hero-logo--image" src={logo} alt="The Biscuit Bar" />
-        </h1>
-        <h2 className="Hero-slogan">Biscuits.&nbsp;Tots.&nbsp;Taps.</h2>
-        <p className="Hero-address">
-          5880 TX-121, Suite 102B<br />Plano, TX 75024
-        </p>
-        <p className="Hero-hours">
-          Sunday 8am-10pm<br />
-          Mon - Wed 8am-11pm<br />
-          Thurs - Sat 8am-Midnight
-        </p>
+        <div className="Hero-overlay" />
+        <div className="Hero-flexWrapper">
+          <div className="Hero-container">
+            <h1 className="Hero-logo">
+              <img
+                className="Hero-logo--image"
+                src={logo}
+                alt="The Biscuit Bar"
+              />
+            </h1>
+            <h2 className="Hero-slogan">Biscuits.&nbsp;Tots.&nbsp;Taps.</h2>
+            <address className="Hero-address">
+              5880 TX-121, Suite 102B<br />Plano, TX 75024<br />
+              <a href="tel:4692382227">469.238.2227</a>
+            </address>
+
+            <p className="Hero-hours">
+              Sunday 8am-10pm<br />
+              Mon - Wed 8am-11pm<br />
+              Thurs - Sat 8am-Midnight
+            </p>
+          </div>
+        </div>
       </section>
     );
   }
