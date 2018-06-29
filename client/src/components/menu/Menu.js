48fd14e06f3e6cac7d26164aeeee36fbb4720de1
Jacob Allen <jedijake@me.com>
2018-06-28 21:19:33 -0500

    Add Menus to menu page

--- client/src/components/menu/Menu.js
+++ client/src/components/menu/Menu.js
@@ -1,7 +1,39 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import "./Menu.css";
+
+import mainMenu from "./assets/main-menu.jpg";
+import drinkMenu from "./assets/drink-menu.jpg";
+import happyHour from "./assets/happy-hour.jpg";
 
 export default class Menu extends Component {
   render() {
-    return <div className="Menu">The goods we have</div>;
+    return (
+      <Fragment>
+        <header id="hero-menu" className="Hero-Menu">
+          <h1 className="Hero-Menu-heading">
+            Mind your biscuits, and life will be&nbsp;gravy.
+          </h1>
+        </header>
+        <main className="Menu">
+          <div className="Menu-wrapper">
+            <img
+              src={mainMenu}
+              alt="Biscuits served daily"
+              className="Menu-main"
+            />
+            <img
+              src={drinkMenu}
+              alt="Drinks served daily"
+              className="Menu-main"
+            />
+            <img
+              src={happyHour}
+              alt="Happy Hour Monday through Friday from 3pm to 6pm"
+              className="Menu-main"
+            />{" "}
+          </div>
+        </main>
+      </Fragment>
+    );
   }
 }
