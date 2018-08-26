fecea818b28e4602c1c32b30b1aedeacc40ae5d8
Jacob Allen <jedijake@me.com>
2018-08-26 14:30:55 -0500

    Update menu hero (#23)

--- client/src/components/menu/Menu.js
+++ client/src/components/menu/Menu.js
@@ -3,6 +3,8 @@ import React, { Component, Fragment } from 'react';
 import Cta from '../Cta/Cta';
 import bbarStyles from '../../styles/bbarStyles';
 
+import heroImgMobile from './assets/menu-hero-mobile.jpg';
+import heroImgDesktop from './assets/menu-hero.jpg';
 import mainMenu from './assets/main-menu.jpg';
 import drinkMenu from './assets/drink-menu.jpg';
 import happyHour from './assets/happy-hour.jpg';
@@ -11,11 +13,21 @@ export default class Menu extends Component {
   render() {
     return (
       <Fragment>
-        <header id="hero-menu" className="Hero-Menu">
+        {/* <header id="hero-menu" className="Hero-Menu">
           <h1 className="Hero-Menu-heading">
             Mind your biscuits, and life will be&nbsp;gravy.
           </h1>
-        </header>
+        </header> */}
+        <img
+          src={heroImgMobile}
+          alt="Mind your biscuits and life will be gravy."
+          className="Menu-heroImg--mobile"
+        />
+        <img
+          src={heroImgDesktop}
+          alt="Mind your biscuits and life will be gravy."
+          className="Menu-heroImg--desktop"
+        />
         <main className="Menu">
           <div className="Menu-wrapper">
             <img
