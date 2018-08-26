7b7ec06a30ebf97ea2f42a68592c379e7b77d3a7
Jacob Allen <jedijake@me.com>
2018-08-26 13:03:04 -0500

    Update 404 page (#21)

--- client/src/components/not-found/NotFound.js
+++ client/src/components/not-found/NotFound.js
@@ -1,8 +1,22 @@
-import React, { Component } from "react";
-// import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import fourOhFour from './assets/404.jpg';
 
 export default class NotFound extends Component {
   render() {
-    return <header>Mind your biscuits and head back home...</header>;
+    return (
+      <header className="NotFound">
+        <img
+          className="NotFound-img"
+          src={fourOhFour}
+          alt="Biscuits not found!"
+        />
+        <h1 className="NotFound-heading">Biscuits not found!</h1>
+        <p className="NotFound-copy">
+          Better head back <Link to="/">home</Link>...
+        </p>
+      </header>
+    );
   }
 }
